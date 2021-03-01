d043846d24ed1bd95d0cdab1e01976867ff3f5e4
ilcheese2 <56899583+ilcheese2@users.noreply.github.com>
2021-02-28 18:08:43 -0800

    Update test.py
    Will display 599 results

--- test.py
+++ test.py
@@ -1,8 +1,35 @@
 import requests
 import json
-items = 10
-url = f"https://clouddata.scratch.mit.edu/logs?projectid=403845454&limit={items}&offset=0"
-data = requests.get(url).json()
+import time 
+import itertools
+
+items = 200
+url = f"https://clouddata.scratch.mit.edu/logs?projectid=12785898&limit={items}&offset=0"
+
 filename = "data.json"
-with open(filename, "w") as a:
-    json.dump(data, a, indent=4)
+
+while True:
+    time.sleep(1)
+    new_data = requests.get(url).json()
+    remove = []
+    with open(filename) as a:
+        data = json.load(a)
+
+    for index, s  in enumerate(new_data):
+        for b in data:
+            if b["timestamp"] == s["timestamp"]:
+                remove.append(index)
+
+    for index in sorted(remove, reverse=True):
+        try:
+            del new_data[index]
+        except IndexError:
+            pass
+
+    data.extend(new_data)
+
+    if(len(data) >= 600):
+        data = data[200:]
+
+    with open(filename, "w") as a:
+        json.dump(data, a, indent=4)
